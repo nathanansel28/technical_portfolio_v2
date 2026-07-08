65a6814aaffba7dde02aa2435782e0eaff7dbf18
Nathan Ansel <nathanansel.edu@gmail.com>
2026-07-08 18:09:07 +0800

    feat: new profile picture and new size

--- components/ProfilePicture.tsx
+++ components/ProfilePicture.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function ProfilePicture() {
   return (
-    <div className="relative h-24 w-24 shrink-0 overflow-hidden rounded-full border border-black/10 sm:h-28 sm:w-28">
+    <div className="relative h-80 w-80 shrink-0 overflow-hidden rounded-full border border-black/10 sm:h-80 sm:w-80">
       <Image
         src="/profile.jpg"
         alt="Portrait of Nathan Ansel"
         fill
-        sizes="112px"
+        sizes="3200px"
         className="object-cover"
         priority
       />
